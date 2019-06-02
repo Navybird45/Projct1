bc0e201f628a3658d315dcf6b099dccb3014b5af
Navybird45 <50809973+Navybird45@users.noreply.github.com>
2019-06-02 19:08:30 -0400

    High Score Feature!
    The high score feature has been added.

--- game2.py
+++ game2.py
@@ -1,5 +1,5 @@
 import random
-
+ 
 def start_game():
     low = 1
     high = 10
@@ -34,6 +34,13 @@ def start_game():
             attempts += 1
             print("You got it!")
             print("You guessed the secret number in {} try(s)!".format(attempts))
+            high_score.sort()
+            if high_score[0] > attempts:
+                high_score.insert(0, attempts)
+                print("You have the new high score!")
+            else:
+                print("The high score is currently {}".format(high_score[0]))
+                
             print("Thanks for playing!")
             replay = input("""Would you like to play again? [y]es or [n]o? """)
             if replay == "n":
@@ -55,6 +62,7 @@ def start_game():
         if guess < 1:
             print ("The number must be between 1 and 10.")
             continue
-    
-
-start_game()
+            
+if __name__ == '__main__':
+    high_score = [15]    
+    start_game()
