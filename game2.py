7300ee7997cb4ef4b636cfed9eddc281be6536c2
Navybird45 <50809973+Navybird45@users.noreply.github.com>
2019-06-04 00:12:01 -0400

    PEP8 compliant now!
    Runs through flake8 with no problem

--- game2.py
+++ game2.py
@@ -2,9 +2,10 @@ import random
 import os
 
 high_score = [7]
-first_place = ["Jim-Bob"] 
+first_place = ["Jim-Bob"]
 low = 1
 high = 10
+
 
 def clear_screen():
     """Looks at system;
@@ -13,30 +14,41 @@ def clear_screen():
     """
     os.system("cls" if os.name == "nt" else "clear")
 
+
 def welcome_to_game():
-    """Welcomes the player to the game and tells the the current high score and holder"""
+    """Welcomes the player to the game and tells the
+    current high score and holder.
+    """
     print("""
     ================================================
     Welcome to the Number Guessing Extravaganza!!!!
     ================================================
     """)
-    
+
     print("""
-    The rules are simple; choose a number between 1 
-    and 10, and we will tell you if the secret 
-    number is higher or lower than that. Try and 
+    The rules are simple; choose a number between 1
+    and 10, and we will tell you if the secret
+    number is higher or lower than that. Try and
     guess the secret number in as few guesses as
     possible!
     """)
-    print("The current high score is {}, held by {}. Try and beat it!".format(high_score[0], first_place[0]))
-    #This is a Test Line 
-    #print("The number is {}".format(secret_number))
-    
+    print("""
+    The current high score is {}, held by {}.
+    Try and beat it!
+    """.format(high_score[0], first_place[0]))
+    # This is a Test Line
+    # print("The number is {}".format(secret_number))
+
+
 def start_game():
-    """Welcomes user, picks a number between the low and high variables, then asks user for a guess;
-    If guess is wrong, tells user if their guess was too low or too high;
-    If guess is right, tells user they won, and if they beat the high score asks them for their name;
-    Puts high score in high_score variable and sets name to first place;
+    """Welcomes user, picks a number between the low and high
+    variables, then asks user for a guess;
+    If guess is wrong, tells user if their guess was too
+    low or too high;
+    If guess is right, tells user they won, and if they beat
+    the high score asks them for their name;
+    Puts high score in high_score variable and sets
+    name to first place;
     Asks user if they would like to play again.
     """
     global high_score
@@ -53,25 +65,28 @@ def start_game():
         if guess == secret_number:
             attempts += 1
             print("You got it!")
-            print("You guessed the secret number in {} try(s)!".format(attempts))
+            print(
+              "You guessed the secret number in {} try(s)!".format(attempts))
             high_score.sort()
             if high_score[0] > attempts:
                 first_place[0] = input("What is your name? ")
                 high_score.insert(0, attempts)
-                
+
                 print("You have the new high score!")
             else:
-                print("The high score is currently {}, held by {}".format(high_score[0], first_place[0]))
-                
+                print("""The high score is currently {},
+                      held by {}""".format(high_score[0], first_place[0]))
+
             print("Thanks for playing!")
-            replay = input("""Would you like to play again? [y]es or [n]o? """)
+            replay = input("""
+                           Would you like to play again? [y]es or [n]o? """)
             if replay == "n":
                 print("Thanks for playing!")
                 break
             if replay == "y":
                 clear_screen()
                 start_game()
-                
+
         if 1 <= guess < secret_number:
             attempts += 1
             print("Too low!")
@@ -84,9 +99,11 @@ def start_game():
             print("The number must be between 1 and 10.")
             continue
         if guess < 1:
-            print ("The number must be between 1 and 10.")
+            print("The number must be between 1 and 10.")
             continue
-            
+
 if __name__ == '__main__':
-    """Starts game if being run from script, not imported"""
+    """Starts game if being run from script,
+    not imported
+    """
     start_game()
